1bbaa8770b4369d948eeadf2d9be0e2aadf252f4
Jeddi212 <jedediahf51@gmail.com>
2022-06-21 22:28:51 +0700

    user input

---
+++ src/let_it_user_input.cr
@@ -0,0 +1,14 @@
+puts "Welcome to The Beatless Sing-Along version 1.0"
+puts "Enter a phrase you want The Beatless to sing"
+puts "> "
+user_input = gets
+
+exit if user_input.nil? # Ctrl+D
+
+default_lyrics = "Na, na, na, na-na-na na" \
+                 " / " \
+                 "Na-na-na na, hey Jude"
+
+lyrics = user_input.presence || default_lyrics
+
+puts "The Beatless are singing: 🎵#{lyrics.upcase}🎶🎸🥁"
